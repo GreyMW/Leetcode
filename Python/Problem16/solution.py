1fa8fd84aa4d36f95aa647b29f99e0731a983d1a
GreyMW <GreyMW@gmail.com>
2022-06-06 11:11:24 -0500

    Restructure

---
+++ Python/Problem16/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        pass
+
+if __name__ == "__main__":
+    print(Solution.threeSumClosest([1,2,3,4,5,6,7], 8))
